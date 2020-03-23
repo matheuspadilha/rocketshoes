bf304ecb4bd06d489a0b8682b866c8b981e150ef
Matheus Padilha <mvpadilha94@gmail.com>
2020-03-22 23:46:45 -0300

    configurate reactotron

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Routes from './routes';
+import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header/index';
 
+import Routes from './routes';
 import store from './store';
 
 function App() {
